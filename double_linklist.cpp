b333f243ff5c134ad8d0c5075261f8f1a6422700
Raunak Sarraf <raunaksarraf2007@gmail.com>
2026-04-03 16:55:55 +0530

    double link list

---
+++ double_linklist.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node*previous;
+
+    Node(int value){
+        data=value;
+        next=NULL;
+        previous=NULL;
+
+    }
+ };
+ class Linklist{
+    private:
+    Node*head;
+
+    public:
+    Linklist(){
+        head=NULL;
+    }
+    void pushfront(int data){
+        Node*newnode=new Node(data);
+        if(head==NULL){
+            head=newnode;
+        }
+        else{
+            newnode->next=head;
+            head->previous=newnode;
+            head=newnode;
+        }
+
+    }
+    void display(){
+        Node*temp=head;
+        if (head==NULL)
+        {
+            cout<<"no node at this point";
+        }
+        else{
+            while(temp!=NULL){
+                cout<<temp->data<<"->"<<"<-";
+                temp=temp->next;
+            }
+        }
+        
+
+    }
+
+ };
+
+int main(){
+    Linklist List;
+    List.pushfront(56);
+
+    List.pushfront(89);
+    List.display();
+    return 0;
+}
